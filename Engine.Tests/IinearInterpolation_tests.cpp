fb9a4120fd3c8f0542a342f8391c2768ba2346fa
Brandon Moretz <bmoretz@ionicsolutions.net>
2017-07-16 14:29:59 -0400

    Added Windows Universal Platform UI Project. Setup basic navigation & layout.

--- Engine.Tests/IinearInterpolation_tests.cpp
+++ Engine.Tests/IinearInterpolation_tests.cpp
@@ -10,35 +10,35 @@ using namespace NumericalMethods;
 
 namespace NumericalTests
 {
-    class LinearInterpolationTests :
-            public ::testing::Test
-    {
-    protected:
-        void SetUp() override
-        {
-            m_x = { 10, 20, 30, 40, 49 },
-            m_y = { 0.6, 0.11, 1.1, 1.62, 1.4 };
-        }
+	class LinearInterpolationTests :
+		public ::testing::Test
+	{
+	protected:
+		void SetUp() override
+		{
+			m_x = { 10, 20, 30, 40, 49 };
+			m_y = { 0.6, 0.11, 1.1, 1.62, 1.4 };
+		}
 
-        void TearDown() override
-        {
-        }
+		void TearDown() override
+		{
+		}
 
-        vector<double> m_x, m_y;
-    };
+		vector<double> m_x, m_y;
+	};
 
-    TEST_F( LinearInterpolationTests, Example1 )
-    {
-        auto 
-            expected_value = 0.803,
-            missing_point = 27.0;
+	TEST_F( LinearInterpolationTests, Example1 )
+	{
+		auto
+			expected_value = 0.803,
+			missing_point = 27.0;
 
-        LinearInterpolation li;
+		LinearInterpolation li;
 
-        li.setPoints( m_x, m_y );
+		li.setPoints( m_x, m_y );
 
-        auto actual_value = li.getValue( missing_point );
+		auto actual_value = li.getValue( missing_point );
 
-        EXPECT_EQ( expected_value, actual_value );
-    }
+		EXPECT_EQ( expected_value, actual_value );
+	}
 }
--- Engine.Tests/PolynomialInterpolation_tests.cpp
+++ Engine.Tests/PolynomialInterpolation_tests.cpp
@@ -10,36 +10,36 @@ using namespace NumericalMethods;
 
 namespace NumericalTests
 {
-    class PolynomialInterpolationTests :
-            public ::testing::Test
-    {
-    protected:
+	class PolynomialInterpolationTests :
+		public ::testing::Test
+	{
+	protected:
 
-        void SetUp() override
-        {
-            m_x = { 10, 20, 30, 40, 49 },
-            m_y = { 0.6, 0.11, 1.1, 1.62, 1.4 };
-        }
+		void SetUp() override
+		{
+			m_x = { 10, 20, 30, 40, 49 };
+			m_y = { 0.6, 0.11, 1.1, 1.62, 1.4 };
+		}
 
-        void TearDown() override
-        {
-        }
+		void TearDown() override
+		{
+		}
 
-        vector<double> m_x, m_y;
-    };
+		vector<double> m_x, m_y;
+	};
 
-    TEST_F( PolynomialInterpolationTests, Example1 )
-    {
-        auto 
-            expected_value = 0.795433,
-            missing_point = 27.0;
+	TEST_F( PolynomialInterpolationTests, Example1 )
+	{
+		auto
+			expected_value = 0.795433,
+			missing_point = 27.0;
 
-        PolynomialInterpolation pi;
+		PolynomialInterpolation pi;
 
-        pi.setPoints( m_x, m_y );
+		pi.setPoints( m_x, m_y );
 
-        auto actual_value = pi.getPolynomial( missing_point );
+		auto actual_value = pi.getPolynomial( missing_point );
 
-        EXPECT_DOUBLE_EQ( expected_value, round_digits( actual_value, 6 ) );
-    }
+		EXPECT_DOUBLE_EQ( expected_value, round_digits( actual_value, 6 ) );
+	}
 }
