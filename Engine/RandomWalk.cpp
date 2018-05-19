f5735dcfa926a3e2069f18c2fc587da41f648e9f
Brandon Moretz <bmoretz@ionicsolutions.net>
2018-05-19 15:19:12 -0400

    Added a random walk generator.

---
+++ Engine/RandomWalk.cpp
@@ -0,0 +1,45 @@
+#include "stdafx.h"
+#include "RandomWalk.h"
+
+using namespace std;
+
+namespace NumericalMethods
+{
+	RandomWalk::RandomWalk( const int size, const double start, const double step )
+		: m_size( size ),
+		m_start( start ),
+		m_step( step )
+	{
+	}
+
+	RandomWalk& RandomWalk::operator=( const RandomWalk &p )
+	{
+		if( this != &p )
+		{
+			m_size = p.m_size;
+			m_start = p.m_start;
+			m_step = p.m_step;
+		}
+
+		return *this;
+	}
+
+	vector<double> RandomWalk::getWalk()
+	{
+		vector<double> walk;
+		auto prev = m_start;
+
+		for( auto index = 0; index < m_size; ++index )
+		{
+			const auto r = rand() % 3;
+
+			auto val = prev;
+
+			if( r == 0 ) val += ( m_step * val );
+			else if( r == 1 ) val -= ( m_step * val );
+
+			walk.push_back( val );
+			prev = val;
+		}
+	}
+}
---
+++ Engine/RandomWalk.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <vector>
+
+namespace NumericalMethods
+{
+	class RandomWalk
+	{
+	public:
+
+		explicit RandomWalk( int size, double start, double step );
+		RandomWalk( const RandomWalk &p ) = default;
+		~RandomWalk() = default;
+		RandomWalk& operator=( const RandomWalk &p );
+
+		std::vector<double> getWalk();
+
+	private:
+		int m_size;
+		double m_start, m_step;
+	};
+}
